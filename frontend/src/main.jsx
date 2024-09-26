ad36ccfe0d652124b386f5ee6168bfbad34cdf7c
adityach0uhan <m4with6x@gmail.com>
2024-09-26 17:29:15 +0530

    added browser router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
+import ChatProvider from './Context/ChatProvider.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <ChakraProvider>
-            <App />
-        </ChakraProvider>
-    </StrictMode>
+    <BrowserRouter>
+        <ChatProvider>
+            <ChakraProvider>
+                <App />
+            </ChakraProvider>
+        </ChatProvider>
+    </BrowserRouter>
 );
